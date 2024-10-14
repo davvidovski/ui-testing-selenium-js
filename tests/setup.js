a529e340dca167f98b6ac5acc3d5f5ba3dc86395
Aleksandar <davidovski.a@yahoo.com>
2024-10-14 18:41:10 +0200

    changed workflows

--- tests/setup.js
+++ tests/setup.js
@@ -1,13 +1,13 @@
 const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+require('chromedriver');  // Automatically loads ChromeDriver
 
 async function createDriver() {
     let chromeOptions = new chrome.Options();
     chromeOptions.addArguments('--no-sandbox');
     chromeOptions.addArguments('--disable-dev-shm-usage');
     chromeOptions.addArguments('--disable-gpu');
-    chromeOptions.addArguments('--remote-debugging-port=9222'); // Useful for debugging
-    chromeOptions.addArguments('--headless'); // Run in headless mode
+    chromeOptions.addArguments('--headless');  // Add headless mode if needed
 
     return await new Builder()
         .forBrowser('chrome')
